cc002dc92fd9c321b0922eea19a05c97a2994831
Jeffh1505 <summerfun233@gmail.com>
2023-10-13 15:10:03 -0400

    commit

--- randomproject2.py
+++ randomproject2.py
@@ -10,8 +10,13 @@ b = encode_binary(a)
 list_to_hash = ""
 for i in b:
     list_to_hash += i
-    c = list_to_hash.encode()
-h = hashlib.new('sha256')
-h.update(c)
-c = h.hexdigest()
-print(c)
+
+c = list_to_hash.encode()
+h1 = hashlib.new('sha256')
+h1.update(c)
+c1 = h1.hexdigest()
+c2 = c1.encode()
+h2 = hashlib.new('sha256')
+h2.update(c2)
+c3 = h2.hexdigest()
+print(c3)
