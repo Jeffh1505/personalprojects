cb91b85f372871c987bce619c48af0a0ecb886b7
Jeffh1505 <summerfun233@gmail.com>
2023-10-21 12:40:27 -0400

    commit

--- PracticePython1.py
+++ PracticePython1.py
@@ -1,2 +1,4 @@
 user_input_name = input("Please input your name: ")
-user_input_age = input("Please input your age: ")
+user_input_age = int(input("Please input your age: "))
+years_to_100 = 2023 + (100 - user_input_age)
+print(user_input_name, "will be 100 years old in", years_to_100)
