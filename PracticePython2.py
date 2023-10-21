e21c1394e2296d77435ba3278ea34a0561be8bc6
Jeffh1505 <summerfun233@gmail.com>
2023-10-21 12:47:34 -0400

    commit

---
+++ PracticePython2.py
@@ -0,0 +1,5 @@
+user_input_number = int(input("Please input a number: "))
+if user_input_number % 2 == 0:
+    print(f"{user_input_number} is even")
+elif user_input_number % 2 == 1:
+    print(f"{user_input_number} is odd")
