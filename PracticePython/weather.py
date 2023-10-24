3fb77985b09653a38cfddb56a17d53ebc283145b
Jeffh1505 <summerfun233@gmail.com>
2023-10-24 17:51:39 +0000

    Oct 24, 2023, 1:51 PM

--- PracticePython/weather.py
+++ PracticePython/weather.py
@@ -1,4 +1,6 @@
 import requests
-url = 'https://api.weather.gov/points/{40.8075},{-73.9625}'
+url = 'https://api.weather.gov/points/40.8075,-73.9625'
 response = requests.get(url)
-print(response)
+if response.status_code == 200:
+    data = response.json()
+print(data)
