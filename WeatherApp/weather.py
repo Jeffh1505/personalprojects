0de48d6661dd72d4c6bb207011889c7051b273d4
Jeffh1505 <summerfun233@gmail.com>
2023-10-24 14:48:17 -0400

    commit

--- WeatherApp/weather.py
+++ WeatherApp/weather.py
@@ -13,4 +13,6 @@ response2 = requests.get(forecast_link)
 if response2.status_code == 200:
     data2 = response2.json()
     forecast = data2['properties']['periods'][0]['detailedForecast']
-print("Today's forecast: ", forecast)
+    print("Today's forecast: ", forecast)
+else:
+    print("Forecast not found")
