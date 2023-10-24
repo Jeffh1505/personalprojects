4e4b82370951f83e7cbdab715df6413f4dbad047
Jeffh1505 <summerfun233@gmail.com>
2023-10-24 19:23:50 +0000

    Oct 24, 2023, 3:23 PM

---
+++ WeatherApp/Weather2.py
@@ -0,0 +1,26 @@
+from urllib import response
+import requests
+user_address = input("Please input your address: ")
+address_url = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={user_address}&benchmark=2020&format=json'
+response1 = requests.get(address_url)
+if response1.status_code == 200:
+    data1 = response1.json()
+else:
+    print("Invalid Location.")
+latitude = data1['result']['coordinate']['y'][:-1]
+longitude = data1['result']['coordinate']['x'][:-1]
+url1 = f'https://api.weather.gov/points/{latitude},{longitude}'
+response2 = requests.get(url1)
+if response2.status_code == 200:
+    data = response.json()
+    forecast_link = data['properties']['forecast']
+else: 
+    print("Invalid location")
+
+response3 = requests.get(forecast_link)
+if response3.status_code == 200:
+    data2 = response2.json()
+    forecast = data2['properties']['periods'][0]['detailedForecast']
+    print("Today's forecast: ", forecast)
+else:
+    print("Forecast not found")
