812ab54600996e43b725b730eac9ef0facb4ec01
Jeffh1505 <summerfun233@gmail.com>
2023-10-24 18:20:18 +0000

    Oct 24, 2023, 2:20 PM

--- PracticePython/weather.py
+++ PracticePython/weather.py
@@ -8,5 +8,5 @@ if response.status_code == 200:
 response2 = requests.get(forecast_link)
 if response2.status_code == 200:
     data2 = response2.json()
-    forecast = data2['']
-print(data2)
+    forecast = data2['properties']['periods'][0]['detailedForecast']
+print(forecast)
