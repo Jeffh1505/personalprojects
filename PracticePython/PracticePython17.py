9f24db8d39c6ef8f109a64732115d12dd4ebf58b
Jeffh1505 <summerfun233@gmail.com>
2023-10-24 14:59:17 +0000

    Oct 24, 2023, 10:59 AM

--- PracticePython/PracticePython17.py
+++ PracticePython/PracticePython17.py
@@ -3,3 +3,6 @@ from bs4 import BeautifulSoup
 
 NYT_homepage = requests.get('https://www.nytimes.com/')
 NYT_homepage_html = NYT_homepage.text
+soup = BeautifulSoup(NYT_homepage_html)
+title = soup.find('span', 'articletitle').string
+print(title)
