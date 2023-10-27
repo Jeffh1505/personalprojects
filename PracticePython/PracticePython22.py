cc31f2075e0ceaccf1989928f28fb9aeea6a1e33
Jeffh1505 <summerfun233@gmail.com>
2023-10-26 22:16:12 -0400

    commit

---
+++ PracticePython/PracticePython22.py
@@ -0,0 +1,17 @@
+result = {}
+name1 = 0 
+name2 = 0
+name3 = 0 
+with open(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PracticePython\nameslist.txt", "r") as file:
+
+    for line in file:
+        line = file.readline()
+        result.setdefault(line, []).append(1)
+for i in range(len(result[line])):
+    name1 += 1
+    name2 += 1
+    name3 += 1    
+
+print(name1)
+print(name2)
+print(name3)
