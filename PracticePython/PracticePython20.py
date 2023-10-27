bc67ceec226e169ab3a58d039393696138855ade
Jeffh1505 <summerfun233@gmail.com>
2023-10-27 00:25:49 +0000

    Oct 26, 2023, 8:25 PM

--- PracticePython/PracticePython20.py
+++ PracticePython/PracticePython20.py
@@ -10,7 +10,7 @@ c = get_number([1,5,9,15,25,47, 360], 3)
 print(c)
 
 
-def get_number_binary(, s):
+def get_number_binary(arr, low, high, x):
     # Check base case
     if high >= low:
  
@@ -23,12 +23,15 @@ def get_number_binary(, s):
         # If element is smaller than mid, then it can only
         # be present in left subarray
         elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
+            return get_number_binary(arr, low, mid - 1, x)
  
         # Else the element can only be present in right subarray
         else:
-            return binary_search(arr, mid + 1, high, x)
+            return get_number_binary(arr, mid + 1, high, x)
  
     else:
         # Element is not present in the array
         return -1
+    
+a = get_number_binary([1,5,9,15,25,47, 360], 1, 360, 3)
+print(a)
