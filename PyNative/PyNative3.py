f953273950bc3bdb083c74b66734c7e75299e580
Jeffh1505 <summerfun233@gmail.com>
2023-11-03 18:34:32 +0000

    Nov 3, 2023, 2:34 PM

--- PyNative/PyNative3.py
+++ PyNative/PyNative3.py
@@ -1,6 +1,9 @@
-user_input = int(input("Please input a number: "))
-s = 0
-for i in range(user_input + 1):
-    s += i
+try:
+    user_input = int(input("Please input a number: "))
+    s = 0
+    for i in range(user_input + 1):
+        s += i
 
-print(s)
+    print(s)
+except ValueError:
+    print("That is not a number")
