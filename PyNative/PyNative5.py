9709528c0a86f6c7076fc518c602e608c430a513
Jeffh1505 <summerfun233@gmail.com>
2023-11-03 18:51:19 +0000

    Nov 3, 2023, 2:51 PM

--- PyNative/PyNative5.py
+++ PyNative/PyNative5.py
@@ -1,5 +1,7 @@
 numbers = [12, 75, 150, 180, 145, 525, 50]
 
 for i in range(len(numbers)):
-    if numbers[i] % 5 == 0:
+    if numbers[i] % 5 == 0 and numbers[i] <= 150:
         print(numbers[i])
+    elif numbers [i] > 500:
+        break
