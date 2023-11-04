81d09c88cfe23eeb7bc36848513634242ddc594e
Jeffh1505 <summerfun233@gmail.com>
2023-11-04 02:26:49 +0000

    Nov 3, 2023, 10:26 PM

--- PyNative/PyNative11.py
+++ PyNative/PyNative11.py
@@ -1,7 +1,7 @@
 prime_numbers_list = []
 for i in range(25, 50):
     for j in range(11):
-        if j * i != i:
+        if j * i != i and j != 0:
             prime_numbers_list.append(i)
 
 print(prime_numbers_list)
