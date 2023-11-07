e99b370895bc95e21f2bf6bf00f8de63fd8aff8c
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 04:58:19 +0000

    Nov 6, 2023, 11:58 PM

--- RecursionPractice/Binary_search.py
+++ RecursionPractice/Binary_search.py
@@ -6,7 +6,7 @@ def binary_search(list, target, low, high):
     middle = (low + high) // 2
     print(middle)
     if target == list[middle]:
-        return list[middle]
+        return middle
     elif target > list[middle]:
         return binary_search(list, target, low, middle + 1)
     else:
