811adf4b13d8ba31ca0fbc7b76eb3d3a9c910d4d
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 03:03:20 +0000

    Nov 6, 2023, 10:03 PM

---
+++ RecursionPractice/sum.py
@@ -0,0 +1,9 @@
+def main():
+# compute and print 1 + 2 + ... + 10
+    print(sum(45))
+def sum(x):
+# you complete this function recursively
+    if x == 1:
+        return 1
+    else: return x + sum(x-1)
+main()
