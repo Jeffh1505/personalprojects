edd42c140c7b3ba6665c6f1509f5a9d498e57bc6
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 01:53:48 +0000

    Nov 6, 2023, 8:53 PM

--- PyNative/PyNative21.py
+++ PyNative/PyNative21.py
@@ -2,6 +2,9 @@ class vehicle:
     def __init__(self, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
+    
+    def __repr__(self) -> str:
+        return f"Vehicle Max Speed: {self.max_speed} \nVehicle Mileage: {self.mileage}"
 
 
 
