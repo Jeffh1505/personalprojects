6028ce208133c76a62068bf19fbcc04b9372381c
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 05:04:01 +0000

    Nov 7, 2023, 12:04 AM

--- RecursionPractice/Binary_search.py
+++ RecursionPractice/Binary_search.py
@@ -2,7 +2,7 @@ def main():
     list = [3,4,5,6,7,8,9,10,11]
     print(list)
     user_target = int(input("Please input a target in the list: "))
-    list_index = binary_search(list, user_target, 3, 11)
+    list_index = binary_search(list, user_target, 0, len(list)-1)
     print(f"{user_target} is at index {list_index} in the list.")
 
 def binary_search(list, target, low, high):
