3cd72a272c800700a06750f443691f860514f02a
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 01:42:06 +0000

    Nov 6, 2023, 8:42 PM

--- PyNative/PyNative19.py
+++ PyNative/PyNative19.py
@@ -1,8 +1,11 @@
 def even_numbers(n, x):
-    if n == x:
-        return n
-    else:
-        return even_numbers(n + 1, x) % 2
+    even_numbers_list = []
+    for i in range(n, x):
+        if i % 2 == 0:
+            even_numbers_list.append(i)
+        else:
+            continue
+    return even_numbers_list
     
 
 print(even_numbers(4, 30))
