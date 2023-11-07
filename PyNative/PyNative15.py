5efac6290cf39a6d466c12bcb378d030fcd5296f
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 01:10:09 +0000

    Nov 6, 2023, 8:10 PM

--- PyNative/PyNative15.py
+++ PyNative/PyNative15.py
@@ -1,8 +1,5 @@
 def showEmployee(name, salary=9000):
-    return name, salary
+    return print(f"{name}, {salary}")
 
-Ben, salary = showEmployee("Ben", 12000)
-Jessa, salary = showEmployee("Jessa")
-
-print(f"Name: {Ben}, {salary}")
-print(f"Name: {Jessa}, {salary}")
+showEmployee("Ben", 12000)
+showEmployee("Jessa")
