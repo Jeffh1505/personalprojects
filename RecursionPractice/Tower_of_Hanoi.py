c558133b6a94800cb859c95353b94178485f5c5b
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 03:52:42 +0000

    Nov 6, 2023, 10:52 PM

---
+++ RecursionPractice/Tower_of_Hanoi.py
@@ -0,0 +1,10 @@
+def tower_of_hanoi(n, start, end, aux):
+    if n == 1:
+        return print(f"Disk 1 moved from {start} to {end}")
+    else:
+        tower_of_hanoi(n - 1, start, end, aux)
+        print(f"Disk {n} moved from {start} to {end}")
+        tower_of_hanoi(n - 1, aux, end, start)
+
+n = int(input("Enter the number of disks: "))
+print(tower_of_hanoi(n,'A','B','C'))
