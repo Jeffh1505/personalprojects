9c6cef53140b0f94f8d913708cf478d0c94f4aab
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 03:06:37 +0000

    Nov 6, 2023, 10:06 PM

--- RecursionPractice/sum.py
+++ RecursionPractice/sum.py
@@ -1,6 +1,7 @@
 def main():
 # compute and print 1 + 2 + ... + 10
-    print(sum(450))
+    user_input = int(input("What number would you like to sum? "))
+    print(sum(user_input))
 def sum(x):
 # you complete this function recursively
     if x == 1:
