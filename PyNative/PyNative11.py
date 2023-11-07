6590cf5e58cd4780f029c30e307e6a10e30df0e0
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 00:45:15 +0000

    Nov 6, 2023, 7:45 PM

--- PyNative/PyNative11.py
+++ PyNative/PyNative11.py
@@ -1,10 +1,8 @@
 prime_numbers_list = []
 for i in range(25, 50):
-    for j in range(1, 11):
-        for c in range(i):
-            if  j * c == i and j != 1:
-                continue
-            else:
-                prime_numbers_list.append(i)
-
+    for j in range(i//2):
+        if i % j == 0:
+            continue
+        else:      
+            prime_numbers_list.append(i)
 print(prime_numbers_list)
