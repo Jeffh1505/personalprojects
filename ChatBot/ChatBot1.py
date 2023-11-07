fd3f67f949982db29f082d906c1815b19848b0af
Jeffh1505 <summerfun233@gmail.com>
2023-11-07 23:54:45 +0000

    Nov 7, 2023, 6:54 PM

--- ChatBot/ChatBot1.py
+++ ChatBot/ChatBot1.py
@@ -54,7 +54,7 @@ class Chatbot:
 
 chatbot = Chatbot()
 exit = True
-print('beavis', 'cheese', 'cow', 'daemon', 'dragon', 'fox', 'ghostbusters', 'kitty',
+print('beavis, cheese ,cow, daemon, dragon, fox, ghostbusters, kitty',
 'meow', 'miki', 'milk', 'octopus', 'pig', 'stegosaurus', 'stimpy', 'trex', 
 'turkey', 'turtle', 'tux')
 chatbot_representation = input("Please pick which animal you would like your Chat bot to look like from the list: ")
