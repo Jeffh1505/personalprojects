cea47ea27bd5f2771c1eaf676d27a86fb46685cc
Jeffh1505 <summerfun233@gmail.com>
2023-11-08 14:04:32 +0000

    Nov 8, 2023, 9:04 AM

--- ChatBot/weather_functionality.py
+++ ChatBot/weather_functionality.py
@@ -1,6 +1,8 @@
 def get_weather():
     import requests
     user_location = input("Please input your address or a city: ")
+    cities = {"New York": "20 W 34th St., New York, NY 10001", "Los Angeles": "1111 S Figueroa St, Los Angeles, CA 90015"
+              , "Chicago": "233 S Wacker Dr, Chicago, IL 60606", "San Francisco": ""}
     address_url = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={user_location}&benchmark=2020&format=json'
     response1 = requests.get(address_url)
     if response1.status_code == 200:
