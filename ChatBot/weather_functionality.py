30dc8f467291e3ca5a3c23193832607dd5c9f22d
Jeffh1505 <summerfun233@gmail.com>
2023-11-08 14:12:10 +0000

    Nov 8, 2023, 9:12 AM

--- ChatBot/weather_functionality.py
+++ ChatBot/weather_functionality.py
@@ -4,7 +4,8 @@ def get_weather():
     cities = {"New York": "20 W 34th St., New York, NY 10001", "Los Angeles": "1111 S Figueroa St, Los Angeles, CA 90015"
               , "Chicago": "233 S Wacker Dr, Chicago, IL 60606", "San Francisco": "600 Montgomery St, San Francisco, CA 94111",
               "Houston": "2800 Post Oak Blvd, Houston, TX 77056", "Miami":" 1 Washington Ave, Miami Beach, FL 33139", 
-              "Philadelphia": ""}
+              "Philadelphia": "520 Chestnut St, Philadelphia, PA 19106", "Seattle": "400 Broad St, Seattle, WA 98109", 
+              "Boston": ""}
     address_url = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={user_location}&benchmark=2020&format=json'
     response1 = requests.get(address_url)
     if response1.status_code == 200:
