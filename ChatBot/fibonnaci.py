03f6038c56bd275f0a7e615156f8cb6a5b6d4929
Jeffh1505 <summerfun233@gmail.com>
2023-11-08 14:54:27 +0000

    Nov 8, 2023, 9:54 AM

---
+++ ChatBot/fibonnaci.py
@@ -0,0 +1,12 @@
+cache = {}
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    if n in cache:
+        return cache[n]
+    result =  fib(n-1) + fib(n-2)
+    cache[n] = result             
+    return result
+
+user_input = int(input("What number of the Fibonnaci sequence would you like? "))    
+print(fib(user_input))
