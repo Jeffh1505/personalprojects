6dea0f110f3f23cee5967f0ac645ffe6819dac95
Jeffh1505 <summerfun233@gmail.com>
2023-11-08 13:52:02 +0000

    Nov 8, 2023, 8:52 AM

---
+++ ChatBot/weather_functionality.py
@@ -0,0 +1,28 @@
+def get_weather():
+    import requests
+    user_address = input("Please input your address: ")
+    address_url = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={user_address}&benchmark=2020&format=json'
+    response1 = requests.get(address_url)
+    if response1.status_code == 200:
+        data1 = response1.json()
+
+    else:
+        return "Invalid Location."
+    latitude = str(data1['result']['addressMatches'][0]['coordinates']['y'])[:-10]
+    longitude = str(data1['result']['addressMatches'][0]['coordinates']['x'])[:-10]
+
+    url1 = f'https://api.weather.gov/points/{latitude},{longitude}'
+    response2 = requests.get(url1)
+    if response2.status_code == 200:
+        data = response2.json()
+        forecast_link = data['properties']['forecast']
+    else: 
+        return "Invalid location"
+
+    response3 = requests.get(forecast_link)
+    if response3.status_code == 200:
+        data2 = response3.json()
+        forecast = data2['properties']['periods'][0]['detailedForecast']
+        return f"Today's forecast: {forecast}"
+    else:
+        return "Forecast not found"
