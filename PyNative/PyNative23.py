8249df70ed41f027650acd18fd0132a2b61d5efc
Jeffh1505 <summerfun233@gmail.com>
2023-11-09 20:26:34 +0000

    Nov 9, 2023, 3:26 PM

--- PyNative/PyNative23.py
+++ PyNative/PyNative23.py
@@ -1,10 +1,13 @@
 class Vehicle:
 
-    def __init__(self, name, max_speed, mileage, color="white"):
+    def __init__(self, name, max_speed, mileage, color="White"):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
         self.color = color
+
+    def __repr__(self):
+        return f"Color: {self.color} \nVehicle name: {self.name} \nVehicle Max Speed: {self.max_speed} mph \nVehicle Mileage: {self.mileage} mpg"
 
 class Bus(Vehicle):
     pass
