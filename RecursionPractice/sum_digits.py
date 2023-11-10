59b1006c5e0da58a5d4b8dd4d24cf13f22e43e8a
Jeffh1505 <summerfun233@gmail.com>
2023-11-10 18:18:45 +0000

    Nov 10, 2023, 1:18 PM

---
+++ RecursionPractice/sum_digits.py
@@ -0,0 +1,12 @@
+def sum_digits(n):
+    sum_of_digits = 0
+    if n == 0:
+        return sum_of_digits
+    right_most_number = n % 10
+    print(right_most_number)
+    sum_of_digits += right_most_number 
+    
+    return sum_digits(sum_of_digits)
+
+
+print(sum_digits(687))
