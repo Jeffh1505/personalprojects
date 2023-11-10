6bb5f9c4729d6843ef631e491a6eb5918d607c3c
Jeffh1505 <summerfun233@gmail.com>
2023-11-10 15:23:54 +0000

    Nov 10, 2023, 10:23 AM

---
+++ RecursionPractice/pattern.py
@@ -0,0 +1,15 @@
+def pattern(n, m, flag=True):
+    if flag == False and m == n:
+        return print(m)
+    if flag == True:
+        if m - 5 > 0:
+            pattern(n, m-5, flag=True)
+        else:
+            flag=False
+            pattern(n,m-5, flag)
+
+    else:
+        pattern(n, m+5)
+
+
+pattern(10, 1)
