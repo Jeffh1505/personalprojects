b7ba4c05c42bd7708b10a55fa5d085af3a1d665a
Jeffh1505 <summerfun233@gmail.com>
2023-11-10 20:06:32 +0000

    Nov 10, 2023, 3:06 PM

---
+++ RecursionPractice/Recamans_sequence.py
@@ -0,0 +1,13 @@
+def recaman(n):
+    reacman_sequence_list = []
+    reacman_sequence_list.append(n)
+    if n == 0:
+        return 0
+    
+    if n > 0 and n not in reacman_sequence_list:
+        return recaman(n-1) - n
+    else:
+        return recaman(n-1) + n
+    
+
+print(recaman(6))
