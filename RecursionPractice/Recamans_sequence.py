ffaeeaad3f055a6d1a2cc7d22eb21c71b5df7dd0
Jeffh1505 <summerfun233@gmail.com>
2023-11-10 20:23:22 +0000

    Nov 10, 2023, 3:23 PM

--- RecursionPractice/Recamans_sequence.py
+++ RecursionPractice/Recamans_sequence.py
@@ -1,16 +1,31 @@
 def recaman(n):
-    reacman_sequence_list = []
-    reacman_sequence_list.append(n)
-    print(reacman_sequence_list)
-    if n == 0:
-        return 0
-    
-    if n > 0 and n not in reacman_sequence_list:
-        return recaman(n-1) - n
-        
-    else:
-         return recaman(n-1) + n
+ 
+    # Create an array to store terms
+    arr = [0] * n
+ 
+    # First term of the sequence
+    # is always 0
+    arr[0] = 0
+    print(arr[0], end=", ")
+ 
+    # Fill remaining terms using
+    # recursive formula.
+    for i in range(1, n):
+     
+        curr = arr[i-1] - i
+        for j in range(0, i):
          
-    
-
-print(recaman(6))
+            # If arr[i-1] - i is
+            # negative or already
+            # exists.
+            if ((arr[j] == curr) or curr < 0):
+                curr = arr[i-1] + i
+                break
+             
+        arr[i] = curr
+        print(arr[i], end=", ")
+ 
+# Driver code
+n = 17
+ 
+recaman(n)
