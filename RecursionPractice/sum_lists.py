54f004589a4308c341d29351cd11047a52f46572
Jeffh1505 <summerfun233@gmail.com>
2023-11-12 15:29:56 +0000

    Nov 12, 2023, 10:29 AM

--- RecursionPractice/sum_lists.py
+++ RecursionPractice/sum_lists.py
@@ -1,9 +1,8 @@
 def sum_lists(li, i=0):
-    if len(list(li)) == 1:
+    if len(li) == 1:
         return li[i]
     
-    else:
-        return li[i] + sum_lists(li[i:], i + 1)
     
+
 
 sum_lists([1, 2, [3,4], [5,6]])
