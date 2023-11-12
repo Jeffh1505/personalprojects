64194f6f44ac3d4c9d8ab227fe6670a64c9a2c9f
Jeffh1505 <summerfun233@gmail.com>
2023-11-12 16:07:41 +0000

    Nov 12, 2023, 11:07 AM

--- RecursionPractice/geometric_series.py
+++ RecursionPractice/geometric_series.py
@@ -2,7 +2,7 @@ def geometric_series(n):
     if n == 0:
         return 1
     else:
-        return 1/(3**n) + 1/(3**geometric_series(n-1))
+        return (1/(3**n)) + (1/(3**geometric_series(n-1)))
     
 
 print(geometric_series(5))
