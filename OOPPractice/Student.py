56f91c71be348a0ea80b1a12770b1c6c81c2aa82
Jeffh1505 <summerfun233@gmail.com>
2023-11-14 19:04:26 +0000

    Nov 14, 2023, 2:04 PM

--- OOPPractice/Student.py
+++ OOPPractice/Student.py
@@ -6,6 +6,19 @@ class Student:
     def __repr__(self):
         return f"Name: {self.name} \nAge: {self.age}"
     
+class Classroom:
+    def __init__(self, students) -> None:
+        self.students = []
+
+    def amount_of_students(self):
+        amount_of_students = 0
+        for student in self.students:
+            amount_of_students += 1
+        return amount_of_students
+    
+    def add_student(self, student):
+        self.students.append()
+
 
 student = Student("Bob", 10)
 
