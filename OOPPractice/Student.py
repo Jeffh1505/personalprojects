f567edb03f86c7cb834cdc77f167981a39efa40e
Jeffh1505 <summerfun233@gmail.com>
2023-11-14 19:10:25 +0000

    Nov 14, 2023, 2:10 PM

--- OOPPractice/Student.py
+++ OOPPractice/Student.py
@@ -19,7 +19,12 @@ class Classroom:
     def add_student(self, student):
         self.students.append(student)
 
+    def __repr__(self) -> str:
+        return f"Students: {self.students} \nNumber of students: {Classroom.amount_of_students(self)}"
 
-student = Student("Bob", 10)
-classroom = Classroom().add_student(student)
-print(classroom.amount_of_students())
+
+students = [Student("Bob", 10), Student("Jill", 9), Student("George", 12), Student("Jane", 8)]
+classroom = Classroom()
+for student in students:
+    classroom.add_student(student)
+print(classroom)
