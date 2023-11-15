5298ff1dcb4ed5b3646fbe80dedcda687fd58c6c
Jeffh1505 <summerfun233@gmail.com>
2023-11-15 13:14:39 +0000

    Nov 15, 2023, 8:14 AM

--- OOPPractice/clock.py
+++ OOPPractice/clock.py
@@ -1,7 +1,9 @@
 class Clock(object):
     def __init__(self, time):
         self.time = time
-    def print_time(self, time):
-        print(time)
-clock = Clock('5:30')
-clock.print_time('10:30')
+    def print_time(self):
+        print(self.time)
+boston_clock = Clock('5:30')
+paris_clock = boston_clock
+paris_clock.time = '10:30'
+boston_clock.print_time()
