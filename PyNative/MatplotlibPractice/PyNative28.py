750de22f2a8237e26a00a92e84e17c19c3271ebc
Jeffh1505 <summerfun233@gmail.com>
2023-11-18 16:47:41 +0000

    Nov 18, 2023, 11:47 AM

--- PyNative/MatplotlibPractice/PyNative28.py
+++ PyNative/MatplotlibPractice/PyNative28.py
@@ -1,11 +1,11 @@
 import csv
 import numpy as np
 import matplotlib as plt 
-month_array = np.zeros((3,4), dtype="uint8") 
+month_array = np.zeros((12,1), dtype="uint8") 
 with open(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\MatplotlibPractice\company_sales_data.csv") as file:
     reader = csv.reader(file)
     for row in reader:
         month,facecream,facewash,toothpaste,bathingsoap,shampoo,moisturizer,total_units,total_profit = row
-        month_array = np.concatenate(month)
+        month_array[row] = month
 
 print(month_array)
