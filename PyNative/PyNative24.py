1d34b9227fa230a8311d7b8026f5e53c1f525943
Jeffh1505 <summerfun233@gmail.com>
2023-11-18 15:30:21 +0000

    Nov 18, 2023, 10:30 AM

--- PyNative/PyNative24.py
+++ PyNative/PyNative24.py
@@ -2,7 +2,5 @@ import numpy as np
 arr = np.array([[64392, 31655], [32579, 0], [49248, 462], [0,0]])
 
 print(arr)
-dimensions = 0
-for dimension in arr.shape:
-    dimensions += 1
-print(f"Array shape:{arr.shape} \nArray dimensions: {dimensions} ")
+
+print(f"Array shape:{arr.shape} \nArray dimensions: {arr.ndim} \nArray Bytes: {arr.itemsize}")
