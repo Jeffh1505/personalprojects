e7a1555067107cd8c80bdf71d006d8e832d2fe9b
Jeffh1505 <summerfun233@gmail.com>
2023-11-18 16:46:20 +0000

    Nov 18, 2023, 11:46 AM

--- PyNative/MatplotlibPractice/PyNative28.py
+++ PyNative/MatplotlibPractice/PyNative28.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 import matplotlib as plt 
-month_array = np.array([]).reshape(12,1)
+month_array = np.zeros((3,4), dtype="uint8") 
 with open(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\MatplotlibPractice\company_sales_data.csv") as file:
     reader = csv.reader(file)
     for row in reader:
