6ebb4d218dfcf4d3c09f5d9ccbe441c8dbe86c12
Jeffh1505 <summerfun233@gmail.com>
2023-11-18 17:07:50 +0000

    Nov 18, 2023, 12:07 PM

--- PyNative/MatplotlibPractice/PyNative28.py
+++ PyNative/MatplotlibPractice/PyNative28.py
@@ -16,5 +16,7 @@ with open(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\Matplotlib
 month_array = np.array(month_list).reshape(12,1)
 profit_array = np.array(profit_list)
 print(profit_array)
+plt.xlabel("Month")
+plt.ylabel("Profit (in USD)")
 plt.plot(month_array, profit_array)
 plt.show()
