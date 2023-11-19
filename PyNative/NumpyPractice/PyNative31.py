99f96349043bfb6c0c85409bda916dfd0f7ae24c
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 23:58:15 +0000

    Nov 19, 2023, 6:58 PM

--- PyNative/NumpyPractice/PyNative31.py
+++ PyNative/NumpyPractice/PyNative31.py
@@ -4,5 +4,8 @@ sampleArray = np.array([[34,43,73],[82,22,12],[53,94,66]])
 
 print(sampleArray)
 
-sorted_array1 = np.sort()
+sorted_array1 = sampleArray[:,sampleArray[1,:].argsort()]
 print(sorted_array1)
+
+sortArrayByColumn = sampleArray[sampleArray[:,1].argsort()]
+print(sortArrayByColumn)
