4e37c55fd8e92c10f83fd3a57ffaf9b2d765221c
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 02:53:32 +0000

    Nov 18, 2023, 9:53 PM

--- MachineLearning1.py
+++ MachineLearning1.py
@@ -1,3 +1,31 @@
-from sklearn import datasets
-digits = datasets.load_digits()
 
+
+import numpy as np
+import matplotlib.pyplot as plt
+# visualizing the data, plotting A.
+
+a =[0, 0, 1, 1, 0, 0,
+   0, 1, 0, 0, 1, 0,
+   1, 1, 1, 1, 1, 1,
+   1, 0, 0, 0, 0, 1,
+   1, 0, 0, 0, 0, 1]
+# B
+b =[0, 1, 1, 1, 1, 0,
+   0, 1, 0, 0, 1, 0,
+   0, 1, 1, 1, 1, 0,
+   0, 1, 0, 0, 1, 0,
+   0, 1, 1, 1, 1, 0]
+# C
+c =[0, 1, 1, 1, 1, 0,
+   0, 1, 0, 0, 0, 0,
+   0, 1, 0, 0, 0, 0,
+   0, 1, 0, 0, 0, 0,
+   0, 1, 1, 1, 1, 0]
+ 
+# Creating labels
+y =[[1, 0, 0],
+   [0, 1, 0],
+   [0, 0, 1]]
+
+plt.imshow(np.array(a).reshape(5, 6))
+plt.show()
