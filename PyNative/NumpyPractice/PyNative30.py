69e2ebd07c6ad637d7e7119842e21621de385e71
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 23:42:25 +0000

    Nov 19, 2023, 6:42 PM

--- PyNative/NumpyPractice/PyNative30.py
+++ PyNative/NumpyPractice/PyNative30.py
@@ -4,14 +4,6 @@ array_to_split = np.array(range(10,34)).reshape(8,3)
 
 print("Original array:",array_to_split)
 
-array1 = array_to_split[:2,:]
-print("Array 1: ",array1)
+splitted_array = np.split(array_to_split, 4)
 
-array2 = array_to_split[2:4,:]
-print("Array 2: ", array2)
-
-array3 = array_to_split[4:6,:]
-print("Array 3: ", array3)
-
-array4 = array_to_split[6:8,:]
-print("Array 4: ", array4)
+print(splitted_array)
