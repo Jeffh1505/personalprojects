51cda4f2c83678e57f76f24c8535f40ad856b18e
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 02:47:39 +0000

    Nov 18, 2023, 9:47 PM

--- MachineLearning1.py
+++ MachineLearning1.py
@@ -1,9 +1,3 @@
-#Load Library
-import numpy as np
-import SciPy
-#Create a Matrix
-matrix = np.array([[0,0],[0,1],[3,0]])
-print(matrix)
-#Create Compressed Sparse Row(CSR) matrix
-matrix_sparse = sparse.csr_matrix(matrix)
-print(matrix_sparse)
+from sklearn import datasets
+digits = datasets.load_digits()
+
