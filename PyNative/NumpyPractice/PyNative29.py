0ba982214a51ff1a59114200fbcf8cc02800b8ac
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 23:33:27 +0000

    Nov 19, 2023, 6:33 PM

--- PyNative/NumpyPractice/PyNative29.py
+++ PyNative/NumpyPractice/PyNative29.py
@@ -4,6 +4,8 @@ arrayOne = np.array([[5, 6, 9], [21 ,18, 27]])
 arrayTwo = np.array([[15 ,33, 24], [4 ,7, 1]])
 arrayThree = arrayOne + arrayTwo
 
+print(arrayThree)
+
 arrayThree = arrayThree**2
 
 print(arrayThree)
