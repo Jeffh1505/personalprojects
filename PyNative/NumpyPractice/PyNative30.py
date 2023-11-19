df2c098782d795bd2e38cb57b6a7aa4f17403ff2
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 23:39:27 +0000

    Nov 19, 2023, 6:39 PM

--- PyNative/NumpyPractice/PyNative30.py
+++ PyNative/NumpyPractice/PyNative30.py
@@ -2,7 +2,10 @@ import numpy as np
 
 array_to_split = np.array(range(10,34)).reshape(8,3)
 
-print(array_to_split)
+print("Original array:",array_to_split)
 
 array1 = array_to_split[:2,:]
-print(array1)
+print("Array 1: ",array1)
+
+array2 = array_to_split[2:4,:]
+print("Array 2: ", array2)
