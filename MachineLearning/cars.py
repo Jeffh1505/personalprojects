c3a4968ed0a25a0f7aec96b1064647eb796997d6
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 03:19:21 +0000

    Nov 18, 2023, 10:19 PM

--- MachineLearning/cars.py
+++ MachineLearning/cars.py
@@ -11,4 +11,15 @@ import scipy as sp
 df = pd.read_csv(r'C:\Users\summe\OneDrive\Desktop\personalprojects\MachineLearning\car data.csv')  
   
 # Checking the first 5 entries of dataset 
-df.head()
+
+
+headers = ["symboling", "normalized-losses", "make",  
+           "fuel-type", "aspiration","num-of-doors", 
+           "body-style","drive-wheels", "engine-location", 
+           "wheel-base","length", "width","height", "curb-weight", 
+           "engine-type","num-of-cylinders", "engine-size",  
+           "fuel-system","bore","stroke", "compression-ratio", 
+           "horsepower", "peak-rpm","city-mpg","highway-mpg","price"] 
+  
+df.columns=headers 
+print(df.head()) 
