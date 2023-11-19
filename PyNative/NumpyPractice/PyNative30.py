f4c268f43e7b7bd2b8d8996452759398b489dca7
Jeffh1505 <summerfun233@gmail.com>
2023-11-19 23:41:09 +0000

    Nov 19, 2023, 6:41 PM

--- PyNative/NumpyPractice/PyNative30.py
+++ PyNative/NumpyPractice/PyNative30.py
@@ -9,3 +9,9 @@ print("Array 1: ",array1)
 
 array2 = array_to_split[2:4,:]
 print("Array 2: ", array2)
+
+array3 = array_to_split[4:6,:]
+print("Array 3: ", array3)
+
+array4 = array_to_split[6:8,:]
+print("Array 4: ", array4)
