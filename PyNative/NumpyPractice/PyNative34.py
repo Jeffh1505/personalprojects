b6dc45a1c0e612f3d72c9a9ac163bc9337e50d66
Jeffh1505 <summerfun233@gmail.com>
2023-11-20 00:25:57 +0000

    Nov 19, 2023, 7:25 PM

---
+++ PyNative/NumpyPractice/PyNative34.py
@@ -0,0 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+array1 = np.array([1,2,3,4,5,6,7,8,9,10])
+
+array2 = np.array([5,12,56,43,67,90,34,22,10,86])
+
+plt.plot(array1, array2)
+
+plt.show()
