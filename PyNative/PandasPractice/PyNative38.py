65f4d1308cd92e535df95813eeef8809b18f06cd
Jeffh1505 <summerfun233@gmail.com>
2023-11-20 20:26:01 +0000

    Nov 20, 2023, 3:26 PM

--- PyNative/PandasPractice/PyNative38.py
+++ PyNative/PandasPractice/PyNative38.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 
 df = pd.read_csv(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\PandasPractice\Automobile_data.csv")
-price = "price"
-print(df[price].max())
+df = df [['company','price']][df.price==df['price'].max()]
+print(df)
 
