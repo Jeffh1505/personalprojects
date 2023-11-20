baf08e9e16c51872e5fd14221c5d51a370547004
Jeffh1505 <summerfun233@gmail.com>
2023-11-20 00:14:03 +0000

    Nov 19, 2023, 7:14 PM

--- PyNative/NumpyPractice/PyNative33.py
+++ PyNative/NumpyPractice/PyNative33.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 sampleArray = np.array([[34,43,73],[82,22,12],[53,94,66]]) 
-newColumn = np.array([[10,10,10]])
+newColumn = np.array([[10,10,10]]).reshape(3,1)
 
 
-print(sampleArray.shape)
-print(newColumn.shape)
+print(sampleArray)
+
 sampleArray = sampleArray[:,:2]
 print(sampleArray)
 
 new_array = np.hstack((sampleArray, newColumn))
+
+print(new_array)
