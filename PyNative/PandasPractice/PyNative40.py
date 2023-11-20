605e1e62bc250e48e4208ad328bda20621bad5a3
Jeffh1505 <summerfun233@gmail.com>
2023-11-20 20:46:16 +0000

    Nov 20, 2023, 3:46 PM

---
+++ PyNative/PandasPractice/PyNative40.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+
+
+df = pd.read_csv(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\PandasPractice\Automobile_data.csv")
+car_Manufacturers = df.groupby('company')
+
+print(car_Manufacturers.count())
