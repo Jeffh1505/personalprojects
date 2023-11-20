a14074407d7b782df35270d43d24b060ea4e1c3c
Jeffh1505 <summerfun233@gmail.com>
2023-11-20 20:36:16 +0000

    Nov 20, 2023, 3:36 PM

--- PyNative/PandasPractice/PyNative39.py
+++ PyNative/PandasPractice/PyNative39.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 df = pd.read_csv(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\PandasPractice\Automobile_data.csv")
-df = df [['company','price']][df.company==df['toyota']]
+df = df [['company']][df.company == df['company']['toyota']]
 print(df)
