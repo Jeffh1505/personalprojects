1133a400c0ae2334574a48c23ca320fc0c23e353
Jeffh1505 <summerfun233@gmail.com>
2023-11-20 20:34:41 +0000

    Nov 20, 2023, 3:34 PM

---
+++ PyNative/PandasPractice/PyNative39.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+
+df = pd.read_csv(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\PandasPractice\Automobile_data.csv")
+df = df [['company','price']][df.company==df['toyota']]
+print(df)
