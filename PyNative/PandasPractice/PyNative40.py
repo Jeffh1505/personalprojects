4b8cb95424f5095e0826854667b6f32def297fb2
Jeffh1505 <summerfun233@gmail.com>
2023-11-20 20:48:08 +0000

    Nov 20, 2023, 3:48 PM

--- PyNative/PandasPractice/PyNative40.py
+++ PyNative/PandasPractice/PyNative40.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 
 df = pd.read_csv(r"C:\Users\summe\OneDrive\Desktop\personalprojects\PyNative\PandasPractice\Automobile_data.csv")
-car_Manufacturers = df.groupby('company')
 
-print(car_Manufacturers.describe())
+
+print(df['company'].value_counts())
