f32391b1c21d5c68e262768d70ea9c40e4c0d360
Jeffh1505 <summerfun233@gmail.com>
2023-11-22 21:02:46 +0000

    Nov 22, 2023, 4:02 PM

--- ChatBot/CowSayGPT2.py
+++ ChatBot/CowSayGPT2.py
@@ -1,21 +1,20 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch
+
 class ChatBot:
     def __init__(self):
-        from transformers import GPT2Tokenizer, GPT2LMHeadModel
         self.model_type = 'gpt2-xl'
-        self.device = 'cuda'
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = GPT2LMHeadModel.from_pretrained(self.model_type)
         self.model.config.pad_token_id = self.model.config.eos_token_id  # suppress a warning
 
     def generate(self, prompt='', num_samples=10, steps=20, do_sample=True):
-        from transformers import GPT2Tokenizer
-        import torch
-
         tokenizer = GPT2Tokenizer.from_pretrained(self.model_type)
         encoded_input = tokenizer(prompt, return_tensors='pt').to(self.device)
         x = encoded_input['input_ids'].expand(num_samples, -1) if prompt else None
 
-        # forward the model `steps` times to get samples, in a batch
-        y = self.model.generate(x, max_new_tokens=steps, do_sample=do_sample, top_k=40)
+        # Forward the model `steps` times to get samples, in a batch
+        y = self.model.generate(x, max_length=steps, do_sample=do_sample, top_k=40)
 
         for i in range(num_samples):
             out = tokenizer.decode(y[i].cpu().squeeze())
@@ -23,6 +22,14 @@ class ChatBot:
             print(out)
 
 
-user_input = input("What would you like to say to the chatbot?: ")
-chatbot = ChatBot()
-chatbot.generate(user_input)
+def main():
+    chatbot = ChatBot()
+    while True:
+        user_input = input("What would you like to say to the chatbot?: ")
+        if user_input.lower() in ['exit', 'quit']:
+            break
+        chatbot.generate(user_input)
+
+
+if __name__ == "__main__":
+    main()
