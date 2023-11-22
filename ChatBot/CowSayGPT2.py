5d6e95e2a0c525e98451f831845ec69f71b46135
Jeffh1505 <summerfun233@gmail.com>
2023-11-22 22:49:18 +0000

    Nov 22, 2023, 5:49 PM

--- ChatBot/CowSayGPT2.py
+++ ChatBot/CowSayGPT2.py
@@ -1,6 +1,8 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 import cowsay
+import math
+import sympy as sym
 class ChatBot:
     def __init__(self):
         self.model_type = 'gpt2-xl'
@@ -8,17 +10,20 @@ class ChatBot:
         self.model = GPT2LMHeadModel.from_pretrained(self.model_type)
         self.model.config.pad_token_id = self.model.config.eos_token_id  # suppress a warning
 
-    def generate(self, prompt='', num_samples=10, steps=20, do_sample=True):
+    #This is the generative portion of the chatbot using the GPT2 model
+    def generate(self, prompt='', num_samples=10, steps=40, do_sample=True):
         tokenizer = GPT2Tokenizer.from_pretrained(self.model_type)
         encoded_input = tokenizer(prompt, return_tensors='pt').to(self.device)
         x = encoded_input['input_ids'].expand(num_samples, -1) if prompt else None
 
         # Forward the model `steps` times to get samples, in a batch
-        y = self.model.generate(x, max_length=30, do_sample=do_sample, top_k=40)
+        y = self.model.generate(x, max_length=steps, do_sample=do_sample, top_k=40)
 
         for i in range(num_samples):
             out = tokenizer.decode(y[i].cpu().squeeze())
         return print(cowsay.get_output_string("cow", out))
+    
+    #Gets the weather for a specified place (Be it a specific address or a city)
     def get_weather(self, user_location):
         import requests
         
@@ -26,7 +31,8 @@ class ChatBot:
                 , "Chicago": "233 S Wacker Dr, Chicago, IL 60606", "San Francisco": "600 Montgomery St, San Francisco, CA 94111",
                 "Houston": "2800 Post Oak Blvd, Houston, TX 77056", "Miami":" 1 Washington Ave, Miami Beach, FL 33139", 
                 "Philadelphia": "520 Chestnut St, Philadelphia, PA 19106", "Seattle": "400 Broad St, Seattle, WA 98109", 
-                "Boston": "206 Washington St, Boston, MA 02109", "Washington DC": "1600 Pennsylvania Avenue NW, Washington, DC 20500"}
+                "Boston": "206 Washington St, Boston, MA 02109", "Washington DC": "1600 Pennsylvania Avenue NW, Washington, DC 20500", 
+                "Columbia University": "116th and Broadway, New York, NY 10027"}
         if user_location in cities.keys():
             user_location = cities[user_location]
         address_url = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={user_location}&benchmark=2020&format=json'
@@ -54,31 +60,72 @@ class ChatBot:
             return print(cowsay.get_output_string("cow",f"Today's forecast: {forecast}"))
         else:
             return print(cowsay.get_output_string("cow","Forecast not found"))
-        
-    def math(self, user_input):
-        x, y, z= user_input.split(" ")
-        x = float(x)
-        z = float(z)
-        if y == "+":
-            print(x + z)
-        elif y == "-":
-            print(x - z)
-        elif y == "*":
-            print(x*z)
-        elif y == "/":
-            print(x/z)
+    #Performs mathematical operations     
+    def basic_calculator(self, user_input):
+        if "**" in user_input:
+            x, z = user_input.split("**")
+            print(cowsay.get_output_string("cow",f"{x} raised to {z} = {x ** z}"))
+        elif "square root" or "sqrt" in user_input:
+            if "sqrt" in user_input:
+                x = user_input.split("sqrt")
+            else:
+                x = user_input.split("square root")
+            
+            print(cowsay.get_output_string("cow", str(math.sqrt(x))))
+        else:
+            x, y, z= user_input.split(" ")
+            x = float(x)
+            z = float(z)
+            if y == "+":
+                print(cowsay.get_output_string("cow",f"{x} + {z} = {x + z}"))
+            elif y == "-":
+                print(cowsay.get_output_string("cow",f"{x} - {z} = {x - z}"))
+            elif y == "*":
+                print(cowsay.get_output_string("cow",f"{x} * {z} = {x * z}"))
+            elif y == "/":
+                print(cowsay.get_output_string("cow",f"{x} / {z} = {x / z}"))
+
+    def calculus_calulator(self, method, function):
+        if method == "derivative":
+            x = sym.symbols('x')
+            derivative = sym.diff(function, x)
+            print(cowsay.get_output_string("cow", f"The derivative of {function} with respect to x is {derivative}"))
+        elif method == "integration":
+            x = sym.symbols('x')
+            check_for_limits = input("Would you like to add limits? (y/n): ")
+            if check_for_limits == 'y':
+                upper_limit = input("What is the upper limit?: ")
+                lower_limit = input("What is the lower limit?: ")
+                definite_ingegral = sym.integrate(function, (x, lower_limit, upper_limit))
+                print(cowsay.get_output_string("cow", f"The integral of {function} over the interval {lower_limit} to {upper_limit} is {definite_ingegral}"))
+            elif check_for_limits == 'n':
+                indefinite_integral = sym.integrate(function, x)
+                print(cowsay.get_output_string("Cow", f"The integral of {function} is {indefinite_integral}"))
+        elif method == "Limit":
+            approaching = input("What is x approaching?: ")
+            limit = sym.limit(function, approaching)
+            
+            
+
 
 
 def main():
     chatbot = ChatBot()
     while True:
-        user_input = input("What would you like to say to the chatbot?: ")
+        user_input = input("What would you like to say to the chatbot?: ").lower()
         if user_input.lower() in ['exit', 'quit']:
             print(cowsay.get_output_string("cow", "Goodbye!"))
             break
         elif user_input == "Get me the weather" or user_input == "weather" or user_input == "What's the weather?":
             user_location = input("Where would you like the weather?: ")
             chatbot.get_weather(user_location)
+        elif "+" in user_input or "-" in user_input or "*" in user_input or "/" in user_input or "**" in user_input or ("sqrt" or "square root") in user_input:
+            chatbot.basic_calculator(user_input)
+        elif user_input == "calculus":
+            function = input("What is the function?: ")
+            method = input("What would you like to do to the function? (Derivative, Integration, Limit):")
+            chatbot.calculus_calulator(method, function)
+        
         else:
             chatbot.generate(user_input)
 
