c8351911f5827cba40dbaa4e464969c197e67d34
Jeffh1505 <summerfun233@gmail.com>
2023-11-22 21:38:21 +0000

    Nov 22, 2023, 4:38 PM

--- ChatBot/CowSayGPT2.py
+++ ChatBot/CowSayGPT2.py
@@ -19,6 +19,41 @@ class ChatBot:
         for i in range(num_samples):
             out = tokenizer.decode(y[i].cpu().squeeze())
         print(cowsay.get_output_string("cow", out))
+    def get_weather(self, user_location):
+        import requests
+        
+        cities = {"New York": "20 W 34th St., New York, NY 10001", "Los Angeles": "1111 S Figueroa St, Los Angeles, CA 90015"
+                , "Chicago": "233 S Wacker Dr, Chicago, IL 60606", "San Francisco": "600 Montgomery St, San Francisco, CA 94111",
+                "Houston": "2800 Post Oak Blvd, Houston, TX 77056", "Miami":" 1 Washington Ave, Miami Beach, FL 33139", 
+                "Philadelphia": "520 Chestnut St, Philadelphia, PA 19106", "Seattle": "400 Broad St, Seattle, WA 98109", 
+                "Boston": "206 Washington St, Boston, MA 02109", "Washington DC": "1600 Pennsylvania Avenue NW, Washington, DC 20500"}
+        if user_location in cities.keys():
+            user_location = cities[user_location]
+        address_url = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={user_location}&benchmark=2020&format=json'
+        response1 = requests.get(address_url)
+        if response1.status_code == 200:
+            data1 = response1.json()
+
+        else:
+            return "Invalid Location."
+        latitude = str(data1['result']['addressMatches'][0]['coordinates']['y'])[:-10]
+        longitude = str(data1['result']['addressMatches'][0]['coordinates']['x'])[:-10]
+
+        url1 = f'https://api.weather.gov/points/{latitude},{longitude}'
+        response2 = requests.get(url1)
+        if response2.status_code == 200:
+            data = response2.json()
+            forecast_link = data['properties']['forecast']
+        else: 
+            return "Invalid location"
+
+        response3 = requests.get(forecast_link)
+        if response3.status_code == 200:
+            data2 = response3.json()
+            forecast = data2['properties']['periods'][0]['detailedForecast']
+            return f"Today's forecast: {forecast}"
+        else:
+            return "Forecast not found"
 
 
 def main():
@@ -28,6 +63,9 @@ def main():
         if user_input.lower() in ['exit', 'quit']:
             print(cowsay.get_output_string("cow", "Goodbye!"))
             break
+        elif user_input == "Get me the weather" or user_input == "weather" or user_input == "What's the weather?":
+            user_location = input("Where would you like the weather?: ")
+            chatbot.get_weather(user_location)
         chatbot.generate(user_input)
 
 
