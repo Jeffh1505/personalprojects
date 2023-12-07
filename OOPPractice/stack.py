ed342fc78bd678018c819c03208ddcd8956d2126
Jeffh1505 <jnh2147@columbia.edu>
2023-12-07 01:30:49 +0000

    Dec 6, 2023, 8:30 PM

--- OOPPractice/stack.py
+++ OOPPractice/stack.py
@@ -8,10 +8,22 @@ class Stack:
     def remove(self):
         self.stack.pop()
     def __repr__(self) -> str:
-        return f"Stack: {self.stack}"
-
-
-
+        if len(self.stack) == 0:
+            return "The stack is empty"
+        else:
+            return f"Stack: {self.stack}"
+        
+class Queue(Stack):
+    def __init__(self) -> None:
+        self.stack = []
+        self.queue = self.stack
+    def remove(self):
+        self.queue.pop(0)
+    def __repr__(self) -> str:
+        if len(self.queue) == 0:
+            return "The queue is empty."
+        else:
+            return f"Queue: {self.queue}"
 
 
 def main():
@@ -23,5 +35,13 @@ def main():
     stack.remove()
     stack.remove()
     print(stack)
+    queue = Queue()
+    queue.add('Hello')
+    queue.add('world')
+    queue.add('!')
+    print(queue)
+    queue.remove()
+    queue.remove()
+    print(queue)
 
 main()
