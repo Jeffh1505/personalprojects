edff1bced0708d340463887e5a9006828505ff9d
Jeffh1505 <jnh2147@columbia.edu>
2023-12-08 14:05:34 +0000

    Dec 8, 2023, 9:05 AM

--- hackerranktest.py
+++ hackerranktest.py
@@ -1,12 +1,12 @@
 N = int(input())
 numbers_for_tuple = input().split(' ')
 tuple_for_list = []
-#for i in range(N):
-    #elements_of_tuple = int(numbers_for_tuple[i])
-    #tuple_for_list.append(elements_of_tuple)
+for i in range(N):
+    elements_of_tuple = float(numbers_for_tuple[i])
+    tuple_for_list.append(elements_of_tuple)
     
-final_tuple = tuple(numbers_for_tuple)
+final_tuple = tuple(tuple_for_list)
 print(final_tuple)
 print(hash(final_tuple))
 
-print(hash((1, 2)))
+print(hash((1, 2, 3, 4, 5)))
