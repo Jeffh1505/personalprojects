711303ac42a45baa244562c73035c88ad1745b09
Jeffh1505 <jnh2147@columbia.edu>
2023-12-09 23:43:24 +0000

    Dec 9, 2023, 6:43 PM

--- testtest.py
+++ testtest.py
@@ -1,7 +1,8 @@
 def rotate_right(message, steps):
     encrypted_string = message
     for i in range(steps):
-        encrypted_string = message[-1] + message[:len(message)-1]
+        encrypted_string[0] = message[-1] 
+        encrypted_string[1:] = message[:len(message)-1]
     return encrypted_string
 
 print(rotate_right('python', 3))
