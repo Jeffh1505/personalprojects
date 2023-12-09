0a35e73901268591d19de31ef3648024b2e88752
Jeffh1505 <jnh2147@columbia.edu>
2023-12-09 23:42:19 +0000

    Dec 9, 2023, 6:42 PM

--- testtest.py
+++ testtest.py
@@ -1,19 +1,7 @@
-class Child:
-    def __init__(self, name, age, toys):
-        self.name = name
-        self.age = age # age in years
-        self.toys = toys # number of toys owned by this child as an integer
+def rotate_right(message, steps):
+    encrypted_string = message
+    for i in range(steps):
+        encrypted_string = message[-1] + message[:len(message)-1]
+    return encrypted_string
 
-
-children = [Child("Charlie",6,20), Child("Idris",7,25), Child("Alex",3,5), \
-Child("Pax",3,12), Child("Zara",7,5)]
-
-
-def sum_toys_per_age(persons):
-    toy_dict = {}
-    for person in persons:
-        toy_dict[person.age] = person.toys
-
-    return toy_dict
-
-print(sum_toys_per_age(children))
+print(rotate_right('python', 3))
