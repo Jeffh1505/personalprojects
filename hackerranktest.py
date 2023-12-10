bdf631f11af2efb4df3abb14497caf8644e0a23d
Jeffh1505 <jnh2147@columbia.edu>
2023-12-10 19:50:49 +0000

    Dec 10, 2023, 2:50 PM

--- hackerranktest.py
+++ hackerranktest.py
@@ -1,27 +1,18 @@
-class Solution:
-	def isSame(self, s):
-		s_list = s.split(s.isnumeric())
-		print(s_list)
-		numeric_indicies = []
-		for i in range(len(s_list)):
-			if s[i].isnumeric():
-				numeric_indicies.append(i)
-			else:
-				continue
-		print(numeric_indicies)
-		length_of_letters = s[:int(numeric_indicies[0])]
-		if length_of_letters == int(s[-1]):
-			return 1
-		else:
-		    return 0
+def pos(n):
+    ## Write the code
+    for i in range(n, 0, -1):
+         print(i-1, end=" ")
     
+def neg(n):
+    ##Write the code
+    for i in range(n,1, 1):
+         print(i, end=" ")
 
-if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		s = input()
-		
-		ob = Solution()	
-		answer = ob.isSame(s)
-		
-		print(answer)
+n = int(input())
+
+if n == 0:
+     print("n is already 0")
+elif n > 0:
+     pos(n)
+elif n < 0:
+     neg(n)
