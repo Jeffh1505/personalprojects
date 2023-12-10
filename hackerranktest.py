e9cecc7b5cc37624e1ebf589d48bf22ddcbb87ec
Jeffh1505 <jnh2147@columbia.edu>
2023-12-10 16:50:37 +0000

    Dec 10, 2023, 11:50 AM

--- hackerranktest.py
+++ hackerranktest.py
@@ -1,13 +1,13 @@
 class Solution:
     def isSame(self, s):
-        if not s.isnumeric():
-            return 0
+        #if not s.isnumeric():
+            #return 0
         
-        length = len(s) - 1 if s[-1] != '0' else len(s) - 2
-        print(length)
+        length = len(s) - 1 if s[-2] != '0' else len(s) - 2
+        
         last_digit = int(s[-1])
-        print(last_digit)
-        return 1 if length == last_digit else 0
+        return length, last_digit
+        #return 1 if length == last_digit else 0
     
 
 if __name__ == '__main__':
