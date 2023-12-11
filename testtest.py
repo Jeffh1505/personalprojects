65b9bc66abbc9d061d68ad727e09a1e0d0c07a02
Jeffh1505 <jnh2147@columbia.edu>
2023-12-11 13:30:32 +0000

    Dec 11, 2023, 8:30 AM

--- testtest.py
+++ testtest.py
@@ -7,6 +7,8 @@ square = np.array([[4, 3, 7],
 def compare_diagonal_sums(square):
     diagonal1_sum = square[0,0] + square[1,1] + square[2,2]
     diagonal2_sum = square[0,2] + square[1,1] + square[2,0]
+    print(diagonal1_sum)
+    print(diagonal2_sum)
     if diagonal1_sum == diagonal2_sum:
         return True
     else:
