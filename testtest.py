9220ce66f59040e419c6e2a7552603ee97c075b7
Jeffh1505 <jnh2147@columbia.edu>
2023-12-11 13:45:46 +0000

    Dec 11, 2023, 8:45 AM

--- testtest.py
+++ testtest.py
@@ -7,15 +7,14 @@ square = np.array([[4, 3, 7],
 def compare_diagonal_sums(square):
     diagonal1_sum = 0
     diagonal2_sum = 0
-    for i in range(0, len(square)):
-        length = len(square)-1
-        diagonal1_sum += square[i,i]
-        diagonal2_sum += square[length-i, length-i]
-        print("diagonal1:",diagonal1_sum)
-        print("diagonal2:",diagonal2_sum)
-    if diagonal1_sum == diagonal2_sum:
-        return True
-    else:
-        return False
+    size = len(square)  # Size of the square matrix
+    print(size)
+    for i in range(size):
+        diagonal1_sum += square[i, i]  # Sum of main diagonal
+        diagonal2_sum += square[i, size - i - 1]
+        print("diagonal1: ",diagonal1_sum)
+        print("diagonal2: ",diagonal2_sum)  # Sum of secondary diagonal
     
+    return diagonal1_sum == diagonal2_sum
+
 print(compare_diagonal_sums(square))
