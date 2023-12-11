f21defefaec21e035ae87879f5fe99108393061a
Jeffh1505 <jnh2147@columbia.edu>
2023-12-11 13:39:20 +0000

    Dec 11, 2023, 8:39 AM

--- testtest.py
+++ testtest.py
@@ -5,8 +5,12 @@ square = np.array([[4, 3, 7],
                    [2, 0, 9]])
 
 def compare_diagonal_sums(square):
-    diagonal1_sum = square[0,0] + square[1,1] + square[2,2]
-    diagonal2_sum = square[0,2] + square[1,1] + square[2,0]
+    diagonal1_sum = 0
+    diagonal2_sum = 0
+    for i in range(0, len(square)):
+        length = len(square)-1
+        diagonal1_sum += square[i,i]
+        diagonal2_sum += square[length-i, length-i]
     print(diagonal1_sum)
     print(diagonal2_sum)
     if diagonal1_sum == diagonal2_sum:
