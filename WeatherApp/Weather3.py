e0411b1b0d6ead1f058f03ac1fd16a1e449f7781
Jeffh1505 <jnh2147@columbia.edu>
2023-12-22 06:04:29 +0000

    Dec 22, 2023, 1:04 AM

--- WeatherApp/Weather3.py
+++ WeatherApp/Weather3.py
@@ -1,4 +1,5 @@
 import requests
+import tkinter as tk
 def get_lat_long(address):
     user_address = address
     address_url = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={user_address}&benchmark=2020&format=json'
@@ -25,6 +26,28 @@ def get_weather(latitude, longitude):
     if response3.status_code == 200:
         data2 = response3.json()
         forecast = data2['properties']['periods'][0]['detailedForecast']
-        print("Today's forecast: ", forecast)
+        return forecast
     else:
         print("Forecast not found")
+
+def weather_app():
+    window = tk.Tk()
+    window.title("My Window")
+    window.geometry("300x300")
+
+    label = tk.Label(window, text="Hello, world!")
+    label.pack()
+
+    entry = tk.Entry(window)
+    entry.pack()
+    user_address = entry.get()
+    latitude, longitude = get_lat_long(user_address)
+    weather = get_weather(latitude, longitude)
+    output_text = tk.Text(window, height=4, width=40)
+    output_text.pack()
+    output_text.delete(1.0, tk.END)  # Clear previous output
+    output_text.insert(tk.END, "The forecast is" + weather)
+   
+
+    window.mainloop()
+    
