5787a944c4c6c2a2f97278d212d0fb934b3fb662
Jeffh1505 <jnh2147@columbia.edu>
2023-12-28 04:37:57 +0000

    Dec 27, 2023, 11:37 PM

--- ArduinoPython/test1.py
+++ ArduinoPython/test1.py
@@ -1,6 +1,10 @@
 import pyfirmata2
+import time
 board = pyfirmata2.Arduino('COM3')
 ledpin = board.get_pin('d:11:p')
 
 for i in range(5):
     ledpin.write(1.0)
+    time.sleep(1)
+    ledpin.write(0.0)
+    time.sleep(1)
