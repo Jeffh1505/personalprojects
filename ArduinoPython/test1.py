953df33b924c68075bb5cb4cc5efcf4ef9d6d2e6
Jeffh1505 <jnh2147@columbia.edu>
2023-12-28 04:25:26 +0000

    Dec 27, 2023, 11:25 PM

--- ArduinoPython/test1.py
+++ ArduinoPython/test1.py
@@ -9,9 +9,9 @@ board.digital[13].mode = pyfirmata.OUTPUT
 
 # Blink the LED 5 times
 for i in range(5):
-    board.digital[13].write(1)  # Turn ON the LED
+    board.digital[10].write(1)  # Turn ON the LED
     time.sleep(1)  # Wait for 1 second
-    board.digital[13].write(0)  # Turn OFF the LED
+    board.digital[10].write(0)  # Turn OFF the LED
     time.sleep(1)  # Wait for 1 second
 
 # Close the serial connection
