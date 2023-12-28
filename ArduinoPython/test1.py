f8d7e0b0107c725c009dcd4a7b5c1cddc46655ef
Jeffh1505 <jnh2147@columbia.edu>
2023-12-28 03:55:40 +0000

    Dec 27, 2023, 10:55 PM

---
+++ ArduinoPython/test1.py
@@ -0,0 +1,18 @@
+from pyfirmata import Arduino, util
+import time
+
+# Establish a serial connection to the Arduino Uno
+board = Arduino('/dev/ttyUSB0')  # Change '/dev/ttyUSB0' to the port your Arduino is connected to
+
+# Define the pin mode as OUTPUT
+board.digital[13].mode = pyfirmata.OUTPUT
+
+# Blink the LED 5 times
+for i in range(5):
+    board.digital[13].write(1)  # Turn ON the LED
+    time.sleep(1)  # Wait for 1 second
+    board.digital[13].write(0)  # Turn OFF the LED
+    time.sleep(1)  # Wait for 1 second
+
+# Close the serial connection
+board.exit()
