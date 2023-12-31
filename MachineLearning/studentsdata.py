98cb28b318746b62425926da4cbaf528c86f1c36
Jeffh1505 <jnh2147@columbia.edu>
2023-12-31 06:50:58 +0000

    Dec 31, 2023, 1:50 AM

--- MachineLearning/studentsdata.py
+++ MachineLearning/studentsdata.py
@@ -9,13 +9,13 @@ students = [f"Student_{i}" for i in range(1, 101)]
 
 # Generating random grades for each student in different subjects
 grades_data = {
-    'Math': np.random.randint(60, 100, size=100),
-    'Science': np.random.randint(60, 100, size=100),
-    'English': np.random.randint(60, 100, size=100)
+    'Math': np.random.randint(60, 100, size=len(students)),
+    'Science': np.random.randint(60, 100, size=len(students)),
+    'English': np.random.randint(60, 100, size=len(students))
 }
 
 # Creating a DataFrame
 grades_df = pd.DataFrame(grades_data, index=students)
 
 # Displaying the first few rows of the dataset
-print(grades_df.head())
+print(grades_df)
