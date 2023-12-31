61ae753f3bbf9e2d1ecbb6f4012c75dbbccb186a
Jeffh1505 <jnh2147@columbia.edu>
2023-12-31 05:22:38 +0000

    Dec 31, 2023, 12:22 AM

--- ChatBot/GoogelGeminitest.py
+++ ChatBot/GoogelGeminitest.py
@@ -1,10 +1,10 @@
 import pathlib
 import textwrap
-
+import os
 import google.generativeai as genai
 
 # Used to securely store your API key
-from google.colab import userdata
+
 
 from IPython.display import display
 from IPython.display import Markdown
@@ -13,3 +13,7 @@ from IPython.display import Markdown
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+GOOGLE_API_KEY=os.getenv('googleapi')
+
+genai.configure(api_key=GOOGLE_API_KEY)
