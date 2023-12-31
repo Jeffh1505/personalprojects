f767ca051f147aee03c00bf1b5b244deea8fa5c0
Jeffh1505 <jnh2147@columbia.edu>
2023-12-31 06:49:38 +0000

    Dec 31, 2023, 1:49 AM

--- MachineLearning/studentsdata.py
+++ MachineLearning/studentsdata.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import numpy as np
 
-# Generating data
+# Generating data for 100 students
 np.random.seed(42)
 
 # Creating student names
-students = ['Student_A', 'Student_B', 'Student_C', 'Student_D', 'Student_E']
+students = [f"Student_{i}" for i in range(1, 101)]
 
 # Generating random grades for each student in different subjects
 grades_data = {
-    'Math': np.random.randint(0, 100, size=len(students)),
-    'Science': np.random.randint(0, 100, size=len(students)),
-    'English': np.random.randint(0, 100, size=len(students))
+    'Math': np.random.randint(60, 100, size=100),
+    'Science': np.random.randint(60, 100, size=100),
+    'English': np.random.randint(60, 100, size=100)
 }
 
 # Creating a DataFrame
 grades_df = pd.DataFrame(grades_data, index=students)
 
-# Displaying the dataset
-print(grades_df)
+# Displaying the first few rows of the dataset
+print(grades_df.head())
