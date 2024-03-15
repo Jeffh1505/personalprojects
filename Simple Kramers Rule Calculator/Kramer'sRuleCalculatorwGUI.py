175b823f7a5c450d94cceae6a0891d8ecc1620bb
Jeffh1505 <jnh2147@columbia.edu>
2024-03-15 20:17:23 +0000

    Mar 15, 2024, 4:17 PM

--- Simple Kramers Rule Calculator/Kramer'sRuleCalculatorwGUI.py
+++ Simple Kramers Rule Calculator/Kramer'sRuleCalculatorwGUI.py
@@ -130,7 +130,30 @@ def CramerGUI():
     c3_label.pack()
     c3_entry = tk.Entry(window)
     c3_entry.pack()
-
+    def calculate_result():
+        choice = entry.get()
+        if choice == "2":
+            x1 = fraction_parse_input(x1_entry.get())
+            x2 = fraction_parse_input(x2_entry.get())
+            y1 = fraction_parse_input(y1_entry.get())
+            y2 = fraction_parse_input(y2_entry.get())
+            c1 = fraction_parse_input(c1_entry.get())
+            c2 = fraction_parse_input(c2_entry.get())
+            result_text.set(two_by_two(x1, x2, y1, y2, c1, c2))
+        elif choice == "3":
+            x1 = fraction_parse_input(x1_entry.get())
+            x2 = fraction_parse_input(x2_entry.get())
+            x3 = fraction_parse_input(x3_entry.get())
+            y1 = fraction_parse_input(y1_entry.get())
+            y2 = fraction_parse_input(y2_entry.get())
+            y3 = fraction_parse_input(y3_entry.get())
+            z1 = fraction_parse_input(z1_entry.get())
+            z2 = fraction_parse_input(z2_entry.get())
+            z3 = fraction_parse_input(z3_entry.get())
+            c1 = fraction_parse_input(c1_entry.get())
+            c2 = fraction_parse_input(c2_entry.get())
+            c3 = fraction_parse_input(c3_entry.get())
+            result_text.set(three_by_three(x1, x2, x3, y1, y2, y3, z1, z2, z3, c1, c2, c3))
     result_label = tk.Label(window, text="Result:")
     result_label.pack()
 
@@ -139,3 +162,9 @@ def CramerGUI():
     result_display.pack()
 
     calculate_button = tk.Button(window, text="Calculate", command=calculate_result)
+    calculate_button.pack()
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    CramerGUI()
