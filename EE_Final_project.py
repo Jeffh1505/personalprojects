457c184f9d7b443f334424bdd32a9b6737c1d02a
Jeffh1505 <jnh2147@columbia.edu>
2024-04-30 17:40:32 +0000

    Apr 30, 2024, 1:40 PM

---
+++ EE_Final_project.py
@@ -0,0 +1,18 @@
+def find_highest_voltages(filename):
+    voltages = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            frequency, voltage = map(float, line.split())
+            voltages[voltage] = frequency
+    
+    highest_voltages = sorted(voltages.keys(), reverse=True)[:2]
+    highest_frequencies = [voltages[voltage] for voltage in highest_voltages]
+    
+    return highest_voltages, highest_frequencies
+
+if __name__ == "__main__":
+    filename = input("Enter the filename: ")
+    highest_voltages, highest_frequencies = find_highest_voltages(filename)
+    print("Highest Output Voltages and Corresponding Frequencies:")
+    for voltage, frequency in zip(highest_voltages, highest_frequencies):
+        print("Voltage:", voltage, "Frequency:", frequency)
