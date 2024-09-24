272fd8eefd6e39de852a63c7ad24141ccbd1847a
Jeffh1505 <jnh2147@columbia.edu>
2024-09-24 14:10:48 +0000

    Sep 24, 2024, 10:10 AM

--- Physics Lab/Coef_restit.py
+++ Physics Lab/Coef_restit.py
@@ -1,3 +1,6 @@
+#This code was developed for the first part of lab 1 for PHYS 1494.
+#It calculates the coefficient of restitution and error for the coefficient of each trial, the unweighted average and the weighted average.
+#Author: Jeffrey Hernandez
 #V_i values
 v_i_raw = [0.499, 0.711, 0.74, 0.608, 0.833, 0.713, 0.563, 0.731, 0.6, 0.612]
 v_i_error = [5.7, 9.3, 4.8, 5.6, 8.1, 5.8, 6.1, 7.7, 5.1, 3.3]
@@ -24,4 +27,4 @@ print(f"Coefficient of restitutions: {coeff_restitution_raw}")
 print(f"Coefficient of restitutions errors: {coeff_restitution_error}")
 
 unweighted_average = sum(coeff_restitution_raw) / len(coeff_restitution_raw)
-print(unweighted_average)
+print(f"Unweighted Average = {unweighted_average}")
