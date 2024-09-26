7ba138f8fb3655aa73dad9f7bc6d19d19eaffe31
Jeffh1505 <jnh2147@columbia.edu>
2024-09-26 14:36:55 +0000

    Sep 26, 2024, 10:36 AM

--- Physics Lab/acceleration.py
+++ Physics Lab/acceleration.py
@@ -65,15 +65,17 @@ def funclin(x, a, b):
 pars, w = opt.curve_fit(funclin, h_list, average_a_list, x0, average_error_list, absolute_sigma=True)
 err = np.sqrt(np.diag(w))
 
-#Assuming L = 1m
-
 
 print("Estimated parameters", pars)
 print("Error calc", err)
 print(type(pars))
 fit = funclin(np.array(h_list), *pars)
 
-print(type(fit))
+g = pars[1] * 1
+sigma_g = err[1] *1
+
+print(f"g = {g} m/s^2")
+print(f"Sigma_g = {sigma_g} m/s^2")
 
 plt.figure(figsize=(10,6))
 
