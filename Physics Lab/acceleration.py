4f208d5b15fbfc20257522f13165676e6f1b5d1f
Jeffh1505 <jnh2147@columbia.edu>
2024-09-28 22:58:11 +0000

    Sep 28, 2024, 6:58 PM

--- Physics Lab/acceleration.py
+++ Physics Lab/acceleration.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt
 
+# Data
 experiement_1_data = [0.0154, 0.0156, 0.0171, 0.0157, 0.0154, 0.0166, 0.0155, 0.0161, 0.0153, 0.0161]
 experiement_1_error = [1.3, 11.0, 1.5, 12.0, 10.0, 13.0, 10.0, 4.4, 12.0, 1.1]
 h_1 = 0.001
@@ -27,67 +28,55 @@ experiement_5_data = [0.0642, 0.0648, 0.0643, 0.0659, 0.0632, 0.0645, 0.0641, 0.
 experiement_5_error = [19.0, 40.0, 19.0, 13.0, 15.0, 10.0, 14.0, 17.0, 10.0,13.0]
 h_5 = 0.0063
 
+# Function for unweighted stats
 def unweighted_stats(a1:list) -> tuple:
-    'This function is meant to compute the unweighted stats of the data'
     unweighted_average = sum(a1)/ len(a1)
-
-    sum_for_sigma = 0
-
-    for i in range(len(a1)):
-        sum_for_sigma += (a1[i] - unweighted_average)**2
-
+    sum_for_sigma = sum((x - unweighted_average)**2 for x in a1)
     sigma = math.sqrt(sum_for_sigma / (len(a1) - 1))
     sigma_average = sigma / math.sqrt(len(a1))
     return unweighted_average, sigma_average
 
+# Data list
 data_list = [experiement_1_data, experiement_2_data, experiement_3_data, experiement_4_data, experiement_5_data]
 h_list = [h_1, h_2, h_3, h_4, h_5]
 
 average_a_list = []
-
 average_error_list = []
 
+# Calculating averages and errors
 for i in range(len(data_list)):
     a_avg, sigma_avg = unweighted_stats(data_list[i])
-    average_a_list.append(a_avg)
-    average_error_list.append(sigma_avg)
+    average_a_list.append(round(a_avg, 2))
+    average_error_list.append(round(sigma_avg, 2))
 
-print(average_a_list)
-print(average_error_list)
+print("Average acceleration values:", average_a_list)
+print("Average error values:", average_error_list)
 
-# Initial Guess 
+# Initial guess for curve fitting
 x0 = np.array([0.0, 0.0])
 
-# Define linear function for fitting,
+# Define linear function for fitting
 def funclin(x, a, b):
     return a + b*x
 
 pars, w = opt.curve_fit(funclin, h_list, average_a_list, x0, average_error_list, absolute_sigma=True)
 err = np.sqrt(np.diag(w))
 
-
-print("Estimated parameters", pars)
-print("Error calc", err)
-print(type(pars))
-fit = funclin(np.array(h_list), *pars)
-
-g = pars[1] * 1
-sigma_g = err[1] *1
+# Calculating g and its error with 2 significant figures
+g = round(pars[1], 2)
+sigma_g = round(err[1], 2)
 
 print(f"g = {g} m/s^2")
 print(f"Sigma_g = {sigma_g} m/s^2")
 
-
+# Plotting the data and fit
+fit = funclin(np.array(h_list), *pars)
 
 plt.figure(figsize=(10,6))
-
-# Using different function, and defining marker and error bar colors, size etc..
-plt.errorbar(h_list, average_a_list, average_error_list, marker='x', ecolor='black',mec='red', linestyle='None',ms=4, mew=4, label="Data") 
+plt.errorbar(h_list, average_a_list, average_error_list, marker='x', ecolor='black', mec='red', linestyle='None', ms=4, mew=4, label="Data") 
 plt.plot(h_list, fit, label="Linear fit")
-# Labels axis
 plt.xlabel('h (m)')
 plt.ylabel('a (m$s^{-2}$)')
 plt.legend(loc='best')
 plt.title('h vs a')
-
 plt.show()
