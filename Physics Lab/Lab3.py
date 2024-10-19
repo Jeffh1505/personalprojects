bf58f7406fdd3aad93f6c95b94a32647c653bbfe
Jeffh1505 <jnh2147@columbia.edu>
2024-10-19 00:55:43 +0000

    Oct 18, 2024, 8:55 PM

--- Physics Lab/Lab3.py
+++ Physics Lab/Lab3.py
@@ -1,2 +1,12 @@
-ten_microfarad = [15, 10, 8,6,5,4.5, 4, 3, 2, 2, 2,1.5,1.25, 1, 1,1,1,1,0.8, 0.5, 0.5,0.4, 0.4, 0.3, 0.25,0.20,0.15,0.1,0.05,0.05,0.01,0.01, 0.01,0.0]
+import numpy as np
+import matplotlib as plt 
+ten_microfarad = [15, 10, 8,6,5,4.5, 4, 3, 2, 2, 2,1.5,1.25, 1, 1,1,1,1,0.8, 0.5, 0.5,0.4, 0.4, 0.3, 0.25,0.20,0.15,0.1,0.05,0.05,0.01,0.01, 0.01,0.01,0.01]
 print(len(ten_microfarad))
+
+def linearize(L: list):
+    new_list = np.array(L)
+    Linearized_list = np.log(new_list)
+    return Linearized_list
+
+print(linearize(ten_microfarad))
+
