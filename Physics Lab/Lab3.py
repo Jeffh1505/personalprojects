27b390b7f707a7ad5c468dfabf88a0876368d5a8
Jeffh1505 <jnh2147@columbia.edu>
2024-10-19 01:06:17 +0000

    Oct 18, 2024, 9:06 PM

--- Physics Lab/Lab3.py
+++ Physics Lab/Lab3.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib as plt 
 ten_microfarad = [15, 10, 8,6,5,4.5, 4, 3, 2, 2, 2,1.5,1.25, 1, 1,1,1,1,0.8, 0.5, 0.5,0.4, 0.4, 0.3, 0.25,0.20,0.15,0.1,0.05,0.05,0.01,0.01, 0.01,0.01,0.01]
 
-twenty_microfarad = [14, 10.25, 9.85, 9, 8, 7, 6, 5.75, 5.25, 5, 4.75, 4]
+twenty_microfarad = [14, 10.25, 9.85, 9, 8, 7, 6, 5.75, 5.25, 5, 4.75, 4,3.95, 3.25, 3.05, 3, 2.85, 2.75,2.25]
 print(len(twenty_microfarad))
 
 def linearize(L: list):
