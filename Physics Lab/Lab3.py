d7d0727a3b86bc11bbd162aa5c965cb8ab1e0aa7
Jeffh1505 <jnh2147@columbia.edu>
2024-10-20 00:50:46 +0000

    Oct 19, 2024, 8:50 PM

--- Physics Lab/Lab3.py
+++ Physics Lab/Lab3.py
@@ -8,7 +8,7 @@ twenty_microfarad_charging = [14, 10.25, 9.85, 9, 8, 7, 6, 5.75, 5.25, 5, 4.75, 
 thirty_microfarad_charging = [15, 10, 9.5, 9.0, 8.75, 8.25, 7.75, 7.25, 7, 6.75, 6, 5.75, 5.5, 5.1, 5, 4.75, 4.5, 4.05, 4, 3.85, 3.5, 3.25, 3, 3, 2.95, 2.85, 2.65, 2.5, 2.45, 2.25, 2.1, 2.05, 2, 1.95, 1.85, 1.75, 1.65, 1.5, 1.45, 1.35, 1.25, 1.15, 1.1, 1.05, 1.01, 1, 1, 1, 1, 1, 1, 1, 1, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.1]
 
 # Discharging data for capacitors (in microamps)
-ten_microfarad_discharging = [13.75, 8, 6.25, 5.5, 3, 2.75, 2, 1.75, 1.25, 1, 0.95, 0.9, 0.85, 0.75, 0.5, 0.45, 0.35, 0.25, 0.2, 0.1]
+ten_microfarad_discharging = [13.75, 8, 6.25, 5.5, 3, 2.75, 2, 1.75, 1.25, 1]
 twenty_microfarad_discharging = [9, 8, 7, 5, 4.95, 4, 3.75, 3.25, 3, 2.75]
 thirty_microfarad_discharging = [10, 9.25, 7.95, 6.85, 6.5, 6, 5.25, 5, 4.75, 4.1]
 
@@ -66,13 +66,16 @@ for i, (time, data, error) in enumerate(zip(time_data, linearized_charging, [err
     plt.figure()
 
     # Plot the linearized data with error bars (10% error)
-    plt.errorbar(time, data, yerr=error * 0.10, fmt='o', label=f'Charging Data Set {(i+1)*10}µF', capsize=3, elinewidth=1, markeredgewidth=1)
+    plt.errorbar(time, data, yerr=error, fmt='o', label=f'Charging Data Set {(i+1)*10}µF', capsize=3, elinewidth=1, markeredgewidth=1)
 
     # Plot the best fit line
     plt.plot(time, fit_line, label=f'Best Fit {(i+1)*10}µF', color='red')
 
+    # Display the slope (b) of the best fit line
+    slope = params[1]
+    plt.title(f'Linearized Charging Data with Best Fit  {(i+1)*10}µF\nSlope = {slope:.4f}')
+    
     # Customize the plot
-    plt.title(f'Linearized Charging Data with Best Fit {(i+1)*10}µF')
     plt.xlabel('Time (seconds)')
     plt.ylabel('ln(Current) (µA)')
     plt.legend()
@@ -93,13 +96,16 @@ for i, (time, data, error) in enumerate(zip(time_data, linearized_discharging, [
     plt.figure()
 
     # Plot the linearized data with error bars (10% error)
-    plt.errorbar(time, data, yerr=error * 0.10, fmt='o', label=f'Discharging Data Set {(i+1)*10}µF', capsize=3, elinewidth=1, markeredgewidth=1)
+    plt.errorbar(time, data, yerr=error, fmt='o', label=f'Discharging Data Set {(i+1)*10}µF', capsize=3, elinewidth=1, markeredgewidth=1)
 
     # Plot the best fit line
     plt.plot(time, fit_line, label=f'Best Fit {(i+1)*10}µF', color='red')
 
+    # Display the slope (b) of the best fit line
+    slope = params[1]
+    plt.title(f'Linearized Discharging Data with Best Fit {(i+1)*10}µF \nSlope = {slope:.4f}')
+
     # Customize the plot
-    plt.title(f'Linearized Discharging Data with Best Fit (Discharging Set {(i+1)*10}µF)')
     plt.xlabel('Time (seconds)')
     plt.ylabel('ln(Current) (µA)')
     plt.legend()
