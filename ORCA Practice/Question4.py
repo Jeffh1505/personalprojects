0887a6eea4a2770c0d54d54c63914fc781ec9d9d
Jeffh1505 <jnh2147@columbia.edu>
2025-03-12 22:33:17 +0000

    Mar 12, 2025, 6:33 PM

---
+++ ORCA Practice/Question4.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+x = np.array([[2, 4, 6], 
+              [8, 10, 12]])
+
+y = np.array([2, 2, 2])
+
+division = x/y 
+
+print(division)
+
+z = np.arange(1,4,1)
+print(z)
+mult = x*z 
+
+print(mult)
