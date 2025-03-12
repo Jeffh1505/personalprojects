e82b6de8604fe38e18da72fbe6154f2d0f222564
Jeffh1505 <jnh2147@columbia.edu>
2025-03-12 21:02:17 +0000

    Mar 12, 2025, 5:02 PM

---
+++ ORCA Practice/Question2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([[3, 7, np.nan, 12], 
+                [15, np.nan, 6, 2], 
+                [np.nan, 4, 8, 10]])
+arr[arr==7] = 14
+print(arr)
+nan_indicies = np.where(arr == np.nan)
+print(nan_indicies)
+max_cols = np.nanmax(arr, axis=1)
+print(max_cols)
