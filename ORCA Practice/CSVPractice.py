79fda189891b9f50bd4226c24ae1417db1049997
Jeffh1505 <jnh2147@columbia.edu>
2025-03-13 00:10:14 +0000

    Mar 12, 2025, 8:10 PM

---
+++ ORCA Practice/CSVPractice.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# Load the CSV file
+df = pd.read_csv(r"C:\Users\summe\OneDrive\Desktop\personalprojects\ORCA Practice\sales_data.csv")
+
+# Display first 5 rows
+print(df.head())
